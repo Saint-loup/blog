85a0082ef58bc8729d6e4d6117e50e50d52cb306
Saint-loup <shakespearien@gmail.com>
2021-04-18 18:29:06 +0200

    recherche
    Former-commit-id: f391c10ddaee1c873c92dc460aad191e0b6d516d [formerly aaf816e35d1e9a0a8091ef84225ef4638910ada9]
    Former-commit-id: f13bf6c33f5e8297345284630b0dd0acfd812ecb

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -5,7 +5,7 @@ const md = require('./markdown.js')
 const elasticlunr = require("elasticlunr");
 require('./lunr.stemmer.support.js')(elasticlunr);
 require('./lunr.fr.js')(elasticlunr);
-
+const removeMd = require('remove-markdown');
 Settings.defaultLocale = "fr";
 
 module.exports = {
@@ -21,30 +21,29 @@ module.exports = {
 		// what fields we'd like our index to consist of
 		var index = elasticlunr(function () {
 			this.use(lunr.fr);
-			this.addField("title", { boost: 10 })
+			this.addField("title", { boost: 8 })
 			this.addField("excerpt", { boost: 5 })
 			this.addField("tags", { boost: 5 })
+			this.addField("content", { boost: 2 })
 
-			this.setRef("id");
+
+			this.setRef("url");
 		})
 
 		// loop through each page and add it to the index
 		collection.forEach((page) => {
+
 			index.addDoc({
-				id: page.url,
+				url: page.url,
 				title: page.template.frontMatter.data.title,
 				excerpt: page.template.frontMatter.data.excerpt,
 				tags: page.template.frontMatter.data.tags,
+				content: removeMd(page.template.frontMatter.content),
 				date: page.template.frontMatter.data.date,
 
 			});
 		});
 
-		console.log(index.search('cognitique', {
-			bool: "OR",
-			expand: true
-		})
-		)
 		return index.toJSON();
 	},
 
