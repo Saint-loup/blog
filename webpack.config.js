22a9d1edb0035b42a94310725306aab13b5f10ec
Your Name <you@example.com>
2021-05-02 14:45:06 +0200

    cache busting pour CSS et JS. Fixes #31

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,36 @@
 const path = require('path')
-
-module.exports = {
+const WebpackAssetsManifest = require('webpack-assets-manifest'); module.exports = {
   entry: {
     main: path.resolve(__dirname, 'src/assets/scripts/main.js'),
     search: path.resolve(__dirname, 'src/assets/scripts/search.js'),
     gallery: path.resolve(__dirname, 'src/assets/scripts/gallery.js'),
-
-
   },
+
+
   output: {
+
     path: path.resolve(__dirname, 'dist/assets/scripts'),
-    //  filename: '[name].min.js?[contenthash]'
-    filename: '[name].min.js'
+    filename: '[name].[contenthash].js',
+    /*    détournement du publicpatch*/
+    publicPath: path.resolve(__dirname, 'src')
+
 
   },
 
-  plugins: [],
   module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+    /*  rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
         },
-      },
-    ],
+      ],*/
   },
+  plugins: [
+    new WebpackAssetsManifest({
+      output: '../../../src/_data/hashes_js.json'
+    }),
+  ],
 }
