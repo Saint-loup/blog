e30614d3307f18d82cb95278f6257f5323e4129f
Your Name <you@example.com>
2021-05-09 18:34:39 +0200

    fix délai à l'ouverture du menu fixes #37
    Former-commit-id: fddff69ad271deeae18a1bc6fd247edc927f875a [formerly 3aba61334c0f39b2500e4adfdbbdb6e349147f48]
    Former-commit-id: 1ca557ae39dd8f33f6b3b878ef198ad09e6e71cd

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,1 +1,42 @@
-const alpine = require('alpinejs')
+/*const alpine = require('alpinejs')
+*/
+document.body.style.setProperty('--navbar-height', document.querySelector('#menu-offcanvas').children.length);
+
+function hideOnClickAway(updatedClassList, mainContent, button) {
+	updatedClassList.remove('anim-nav-opened')
+	mainContent.classList.remove('anim-nav-opened')
+	button[0].classList.remove('hidden')
+	button[1].classList.add('hidden')
+	mainContent.removeEventListener("click", hideOnClickAway)
+
+}
+
+const button = document.querySelector('#menu-bar button #trigger-zone')
+
+button.addEventListener("click", function () {
+	const mainContent = document.querySelector('#main-content')
+
+	const offCanvas = document.querySelector('#menu-offcanvas')
+	const button = document.querySelectorAll('#menu-bar button svg')
+
+	button[0].classList.toggle('hidden')
+	button[1].classList.toggle('hidden')
+	offCanvas.classList.toggle('anim-nav-opened')
+	mainContent.classList.toggle('anim-nav-opened')
+
+	const updatedClassList = document.querySelector('#menu-offcanvas').classList
+	if ([...updatedClassList].includes('anim-nav-opened')) {
+		mainContent.addEventListener("click", hideOnClickAway.bind(this, updatedClassList, mainContent, button))
+
+	}
+	else {
+		mainContent.removeEventListener("click", hideOnClickAway)
+
+
+	}
+
+
+
+
+
+})
