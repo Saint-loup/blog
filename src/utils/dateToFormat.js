eee54633358153d07ac453cb466d0658847a9447
Saint-loup <shakespearien@gmail.com>
2021-05-17 21:40:14 +0200

    fix

--- src/utils/dateToFormat.js
+++ src/utils/dateToFormat.js
@@ -1,8 +1,17 @@
 const { DateTime } = require('luxon')
 
 module.exports = function dateToFormat(date, format) {
-	String(date)
-	return DateTime.fromISO(date, {
-		zone: 'utc',
-	}).toFormat(String(format))
+
+	if (typeof date === 'string') {
+		return DateTime.fromISO(date, {
+			zone: 'utc',
+		}).toFormat(String(format))
+	}
+	else {
+		return DateTime.fromJSDate(date, {
+			zone: 'utc',
+		}).toFormat(String(format))
+	}
+
 }
+
