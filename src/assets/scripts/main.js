dd270a2fb33f7650900df1ce566af92ed4ec11f3
Your Name <you@example.com>
2021-06-28 17:32:02 +0200

    test contact

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -4,3 +4,33 @@ require('./nav.js')();
 
 require('./spin.js')();
 
+const axios = require('axios').default;
+document.querySelector("#contact").addEventListener('submit', async function () {
+	event.preventDefault();
+	var data = new FormData(this);
+	console.log(data.entries());
+
+	const res = await fetch('https://nuage.toutcequibouge.net/ocs/v2.php/apps/forms/api/v1/submission/insert', {
+		method: 'POST',
+		mode: 'cors',
+		referrerPolicy: 'no-referrer',
+		headers: {
+			'Content-Type': 'application/json',
+			'OCS-APIRequest': 'true',
+
+		},
+		body: data
+	});
+})
+
+document.querySelector("#contact").addEventListener('formdata', (e) => {
+	console.log('formdata fired');
+
+	// Get the form data from the event object
+	let data = e.formData;
+	for (var value of data.values()) {
+		console.log(value);
+	}
+
+
+});
