a545c7bf38803fba50e76696411d45752d524615
Your Name <you@example.com>
2021-06-28 11:54:50 +0200

    remplacement menu off-canvas par menu scrollant

--- src/assets/scripts/nav.js
+++ src/assets/scripts/nav.js
@@ -1,41 +1,36 @@
 module.exports = function () {
-	document.body.style.setProperty('--navbar-height', document.querySelector('#menu-offcanvas').children.length);
 
-	function hideOnClickAway(updatedClassList, mainContent, buttonState) {
-		updatedClassList.remove('anim-nav-opened')
-		mainContent.classList.remove('anim-nav-opened')
-		buttonState[0].classList.remove('hidden')
-		buttonState[1].classList.add('hidden')
-		button.toggleAttribute('aria-expanded')
-
-		mainContent.removeEventListener("click", hideOnClickAway)
-
-	}
+	window.scrollBy({
+		top: document.querySelector('#menu-offcanvas').getBoundingClientRect().height,
+	});
 
 	const button = document.querySelector('#menu-bar button #trigger-zone')
-
 	button.addEventListener("click", function () {
-		const mainContent = document.querySelector('#content')
 
 		const offCanvas = document.querySelector('#menu-offcanvas')
 		const buttonState = document.querySelectorAll('#menu-bar button svg')
 		const button = document.querySelector('#menu-bar button')
-
 		buttonState[0].classList.toggle('hidden')
 		buttonState[1].classList.toggle('hidden')
 		button.toggleAttribute('aria-expanded')
-		offCanvas.classList.toggle('anim-nav-opened')
-		mainContent.classList.toggle('anim-nav-opened')
+
 
 		const updatedClassList = document.querySelector('#menu-offcanvas').classList
-		if ([...updatedClassList].includes('anim-nav-opened')) {
-			mainContent.addEventListener("click", hideOnClickAway.bind(this, updatedClassList, mainContent, buttonState, button))
-
+		if (![...updatedClassList].includes('anim-nav-opened')) {
+			offCanvas.classList.add('anim-nav-opened')
+			window.scrollTo({
+				top: 0,
+				behavior: 'smooth'
+			});
 		}
 		else {
-			mainContent.removeEventListener("click", hideOnClickAway)
-
+			offCanvas.classList.remove('anim-nav-opened')
+			window.scrollBy({
+				behavior: 'smooth',
+				top: document.querySelector('#menu-offcanvas').getBoundingClientRect().height,
+			});
 		}
+
 
 	})
 
