8c29f20ed3f95f4c48b7a8f2bf4951ea5f5b4dcd
Saint-loup <shakespearien@gmail.com>
2021-07-06 21:37:01 +0200

    fix balise meta

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -5,12 +5,11 @@ module.exports = {
 	env: process.env.NODE_ENV || 'development ',
 	siteURL: 'https://toutcequibouge.net/',
 	siteName: "Tout ce qui bouge",
-	siteDescription:
-		'Veille et réflexion sur la technologie, ses usages et mésusages.',
+	siteDescription: 'Veille et réflexion sur la technologie, ses usages et mésusages.',
 	image: 'assets/UI/linotype.png',
 	lang: 'fr',
 	locale: 'fr_FR',
-	author: 'Baptiste',
+	author: 'Baptiste Roullin',
 	authorEmail: '',
 	zoteroProfileID: "5883126",
 
